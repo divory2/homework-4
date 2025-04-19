8c71bce7db4fe9717d89ad2d3c9ef41bfaf6ab4d
Devon  Ivory <devonivory@Devons-MacBook-Air.local>
2025-04-18 20:01:10 -0400

    added splash screen logic to main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:homework_4/splashscreen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Firebase Auth Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(title: 'Firebase Auth Demo'),
+      home: SplashScreen(),
     );
   }
 }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,18 +1,58 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:homework_4/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:homework_4/main.dart';           // For MyHomePage
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 2), () {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), _navigate);
+  }
+
+  void _navigate() {
+    final user = _auth.currentUser;
+
+    if (user != null) {
+      // User is logged in
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => ProfileScreen()),
+      );
+    } else {
+      // Not logged in
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (_) => MyHomePage(title: "Firebase Auth Demo")),
       );
-    });
+    }
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Welcome to Chat App")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 80),
+            SizedBox(height: 16),
+            Text(
+              "Welcome to Chat App",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
     );
   }
 }
